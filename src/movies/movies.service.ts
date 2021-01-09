4c2786d2ba6d031a924702b979ad1728c4011a09
1eedaegon <d8726243@gmail.com>
2021-01-09 19:34:19 +0900

    feat: add CRUD & improve code quality with eslint

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ${id} not found.`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: string): boolean {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    return true;
+  }
+
+  create(movieData): boolean {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+    return true;
+  }
+}
